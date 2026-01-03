08f0cafc9ed6f9de008ff4bcd147049bef502490
Daniel Felipe <dbfelipe24@gmail.com>
2026-01-02 23:57:11 -0500

    allowing for refresh on dashboard screen

--- server/index.js
+++ server/index.js
@@ -1,54 +1,68 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import helmet from 'helmet';
-import morgan from 'morgan';
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import morgan from "morgan";
 import clientRoutes from "./routes/client.js";
 import generalRoutes from "./routes/general.js";
 import managementRoutes from "./routes/management.js";
 import salesRoutes from "./routes/sales.js";
-
+import path from "path";
 
 // data imports from our mock data
 import User from "./models/User.js";
-import Product from "./models/Product.js"
+import Product from "./models/Product.js";
 import ProductStat from "./models/ProductStat.js";
-import Transaction from './models/Transaction.js';
+import Transaction from "./models/Transaction.js";
 import OverallStat from "./models/OverallStat.js";
-import { dataUser, dataProduct, dataProductStat, dataTransaction, dataOverallStat, dataAffiliateStat, } from "./data/index.js";
-import AffiliateStat from './models/AffiliateStat.js';
+import {
+  dataUser,
+  dataProduct,
+  dataProductStat,
+  dataTransaction,
+  dataOverallStat,
+  dataAffiliateStat,
+} from "./data/index.js";
+import AffiliateStat from "./models/AffiliateStat.js";
 
 /*CONFIGURATION */
 
 dotenv.config();
 const app = express();
+const __dirname = path.resolve();
+
 app.use(express.json());
 app.use(helmet());
-app.use(helmet.crossOriginResourcePolicy({policy: "cross-origin"}));
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
+//serve static files
+app.use(express.static(path.join(__dirname, "client", "build"))); // adjust path
 
 /*ROUTES */
 
 app.use("/client", clientRoutes);
 app.use("/general", generalRoutes);
-app.use("/management", managementRoutes)
-app.use("/sales", salesRoutes)
+app.use("/management", managementRoutes);
+app.use("/sales", salesRoutes);
 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 /*MONGOOSE SETUP*/
 
-const PORT = process.env.PORT || 9000
+const PORT = process.env.PORT || 9000;
 mongoose
-    .connect(process.env.MONGO_URL, {
+  .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-})
-.then(() => {
+  })
+  .then(() => {
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
     /**ONLY ADD DATA ONE TIME */
     //AffiliateStat.insertMany(dataAffiliateStat);
@@ -58,10 +72,5 @@ mongoose
     //ProductStat.insertMany(dataProductStat);
     //Transaction.insertMany(dataTransaction);
     /**Connecting data to mongoDatabase */
-
-})
-.catch((error ) => console.log(`${error} did not connect`));
-
-
-
-
+  })
+  .catch((error) => console.log(`${error} did not connect`));
